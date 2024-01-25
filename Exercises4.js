f096110301409bf4eb870c469b27db074fdd24e4
Miguelmdfg <miguelfuenmayor2002@gmail.com>
2024-01-25 09:01:09 -0400

    actualizacion

--- Exercises4.js
+++ Exercises4.js
@@ -75,32 +75,19 @@ const ultimaEmpresa = itCompanies[itCompanies.length -1];
 
 10.//Imprime cada empresa.//
 
-
+itCompanies.forEach(company => {
+    console.log(company);
+});
 
 11.//Cambie el nombre de cada empresa a mayúsculas uno por uno e imprímalos.//
+
+itCompanies.forEach(company => {
+    const empresaEnMayusculas = company.toUpperCase();
+    console.log(empresaEnMayusculas);
+});
 
 12./*Imprime el array como una oración: Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon 
 son grandes empresas de TI.*/
 
-13./*Compruebe si existe una determinada empresa en el array itCompanies. Si existe, retorna la empresa; 
-de lo contrario, retorna la empresa no existe*/
-
-14.//Filtre las empresas que tienen más de una 'o' sin el método filter()//
-
-15.//Ordene el array usando el método sort()//
-
-16.//Invierte la array usando el método reverse()//
-
-17.//Cortar las primeras 3 empresas del array//
-
-18.//Cortar las últimas 3 empresas del array//
-
-19.//Cortar la empresa o empresas intermedias de TI del array//
-
-20.//Eliminar la primera empresa de TI del array//
-
-21.//Eliminar la empresa o empresas intermedias de TI del array//
-
-22.//Elimine la última empresa de TI del array//
-
-23.//Eliminar todas las empresas de TI//
+const companiesSentence = itCompanies.join(', ');
+console.log(`${companiesSentence} son grandes empresas de TI.`);
