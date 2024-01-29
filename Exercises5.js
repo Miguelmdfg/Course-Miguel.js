5e21ba06717f6ee261160b6436ee14e81421eb92
Miguel Fuenmayor <miguelfuenmayor2002@gmail.com>
2024-01-29 09:06:19 -0400

    actualizacion

---
+++ Exercises5.js
@@ -0,0 +1,61 @@
+1.//Itera de 0 a 10 usando el bucle for, haga lo mismo usando los bucles while y do while//
+
+//For
+
+for (let i = 0; i <= 10; i++);
+console.log(i);
+
+//while
+
+let a = 0;
+
+while (a <= 10) {
+    console.log(a);
+    a++;
+}
+
+//do while
+
+let b = 0;
+
+do {
+    console.log(b);
+    b++;
+} while (b <= 10);
+
+2.//Itera 10 to 0 usando el bucle for, haga lo mismo usando los bucles while y do while//
+
+//for
+
+for (let f = 10; f >= 0; f--);
+console.log(f);
+
+//while
+
+let c = 10;
+
+while (c >= 0) {
+    console.log(c);
+    c--;
+}
+
+//do while
+
+let e = 10;
+
+do {
+    console.log(e);
+    e--;
+} while (e >= 0);
+
+3.//Itera de 0 a n usando el bucle for//
+
+4.//Escribe un bucle que haga el siguiente patrón usando console.log()://
+/*  #
+    ##
+    ###
+    ####
+    #####
+    ######
+    ####### */
+
